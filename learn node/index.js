f607c3d70d3c254d7df1350f6385725f6e79e334
wingstech-jayate <95604984+wingstech-jayate@users.noreply.github.com>
2021-12-25 23:32:47 +0530

    Add files via upload

---
+++ learn node/index.js
@@ -0,0 +1,10 @@
+const fs=require('fs');
+// fs.mkdirSync("thapa")
+// fs.writeFileSync('thapa/bio.txt','my name is vinod thapa');
+// fs.appendFileSync('thapa/bio.txt',"plz subscribe");
+// const data=fs.readFileSync('thapa/bio.txt',"utf-8")
+// const p=data.toString();
+// fs.renameSync("thapa/bio.txt","thapa/mybio.txt")
+// console.log(data,"data");
+// fs.unlinkSync("thapa/mybio.txt")
+fs.rmdirSync("thapa")
